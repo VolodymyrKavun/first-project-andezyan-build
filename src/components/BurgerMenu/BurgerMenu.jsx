595feff62f98a141121b703c2550ac6e4f19ef36
VolodymyrKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2022-10-19 22:33:11 +0300

    change_burger_menu

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,54 +1,31 @@
 import React from 'react';
-import css from './BurgerMenu.module.css';
-import { NavLinkStyled } from 'components/Header/Header.styled';
-import HeaderContactsInfo from 'components/HeaderContactsInfo';
+// import css from './BurgerMenu.module.css';
+import css from './NewStyle.module.css';
+import LinksNav from 'components/LinksNav';
 
 const BurgerMenu = () => {
   return (
     <>
-      <div className={css.menuWrap}>
+      <div id={css.menu}>
+        <input type="checkbox" id={css.chekboxBurger} />
+        <span id={css.buttonBurger}></span>
+        <nav>
+          <LinksNav />
+        </nav>
+      </div>
+
+      {/* todo */}
+      {/* <div className={css.menuWrap}>
         <input type="checkbox" className={css.toggler} />
         <div className={css.hamburger}>
           <div></div>
         </div>
         <div className={css.menu}>
           <div>
-            <div>
-              <ul>
-                <li>
-                  <NavLinkStyled to="/">Головна</NavLinkStyled>
-                </li>
-                <li>
-                  <NavLinkStyled to="/home">Gallery</NavLinkStyled>
-                </li>
-                <li>
-                  <NavLinkStyled to="/login">Login</NavLinkStyled>
-                </li>
-                <li>
-                  <NavLinkStyled to="/signUp">Registration</NavLinkStyled>
-                </li>
-                <li>
-                  <HeaderContactsInfo />
-                </li>
-              </ul>
-            </div>
+            <LinksNav />
           </div>
         </div>
-      </div>
-
-      {/* <header className={css.showcase}>
-        <div className={css.container.showcaseInner}>
-          <h1>Welcome</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-            maiores sint impedit delectus quam molestiae explicabo cum facere
-            ratione veritatis.
-          </p>
-          <a href="/" className={css.btn}>
-            Read More
-          </a>
-        </div>
-      </header> */}
+      </div> */}
     </>
   );
 };
