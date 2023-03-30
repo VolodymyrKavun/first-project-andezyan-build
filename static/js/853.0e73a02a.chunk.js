5cdfe360b80b5234cfa038194b52155c401cc1bb
VolodymyrKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2023-03-30 20:44:55 +0000

    Deploying to gh-pages from @ VolodymyrKavun/first-project-andezyan-build@f44f8664917a39f8376871c45a0cc38a12265b61 🚀

--- static/js/853.0e73a02a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[853],{3853:function(t,e,r){r.d(e,{Imn:function(){return o}});var n=r(9983);function o(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"}},{tag:"polyline",attr:{points:"22,6 12,13 2,6"}}]})(t)}}}]);
-//# sourceMappingURL=853.0e73a02a.chunk.js.map
